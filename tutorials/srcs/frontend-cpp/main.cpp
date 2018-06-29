18f8e1ae12f9e37d05c1ab4350f622aeacbc5155
Tomáš Malý <tomas.maly@melown.com>
2018-06-29 10:18:30 +0200

    update source for vts-browser-minimal example

--- tutorials/srcs/frontend-cpp/main.cpp
+++ tutorials/srcs/frontend-cpp/main.cpp
@@ -25,23 +25,26 @@
  */
 
 #include <vts-browser/map.hpp>
-#include <vts-browser/draws.hpp>
 #include <vts-browser/options.hpp>
 #include <vts-browser/log.hpp>
 #include <vts-browser/fetcher.hpp>
 #include <vts-renderer/renderer.hpp>
+
+#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
 SDL_Window *window;
 SDL_GLContext renderContext;
+vts::renderer::Renderer render;
 std::shared_ptr<vts::Map> map;
-vts::renderer::RenderOptions renderOptions;
 bool shouldClose = false;
 
 void updateResolution()
 {
-    SDL_GL_GetDrawableSize(window, &renderOptions.width, &renderOptions.height);
-    map->setWindowSize(renderOptions.width, renderOptions.height);
+    vts::renderer::RenderOptions &ro = render.options();
+    SDL_GL_GetDrawableSize(window, (int*)&ro.width,
+                                   (int*)&ro.height);
+    map->setWindowSize(ro.width, ro.height);
 }
 
 int main(int, char *[])
@@ -60,21 +63,20 @@ int main(int, char *[])
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    // use OpenGL version 3.0 core profile
+    // use OpenGL version 3.3 core profile
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                         SDL_GL_CONTEXT_PROFILE_CORE);
 
     // create window
     vts::log(vts::LogLevel::info3, "Creating window");
     {
-        // the created window should fill the whole screen
-        SDL_DisplayMode displayMode;
-        SDL_GetDesktopDisplayMode(0, &displayMode);
-        window = SDL_CreateWindow("vts-browser-minimal",
-                  0, 0, displayMode.w, displayMode.h,
-                  SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow("vts-browser-minimal-cpp",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            800, 600,
+            SDL_WINDOW_MAXIMIZED | SDL_WINDOW_OPENGL
+            | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
     }
     if (!window)
     {
@@ -91,30 +93,29 @@ int main(int, char *[])
 
     // notify the vts renderer library on how to load OpenGL function pointers
     vts::renderer::loadGlFunctions(&SDL_GL_GetProcAddress);
+
     // and initialize the renderer library
     // this will load required shaders and other local files
-    vts::renderer::initialize();
+    render.initialize();
 
     // create instance of the vts::Map class
-    map = std::make_shared<vts::Map>(vts::MapCreateOptions());
+    map = std::make_shared<vts::Map>(vts::MapCreateOptions(),
+                vts::Fetcher::create(vts::FetcherOptions()));
 
     // set required callbacks for creating mesh and texture resources
-    // we use the standard callbacks provided by the rendering library
-    map->callbacks().loadTexture = std::bind(&vts::renderer::loadTexture,
-                std::placeholders::_1, std::placeholders::_2);
-    map->callbacks().loadMesh = std::bind(&vts::renderer::loadMesh,
-                std::placeholders::_1, std::placeholders::_2);
-
-    // initialize the resource processing with default fetcher
-    map->dataInitialize(vts::Fetcher::create(vts::FetcherOptions()));
+    render.bindLoadFunctions(map.get());
 
     // initialize the render preparation component of the map
     updateResolution();
     map->renderInitialize();
+    map->dataInitialize();
 
     // configure an url to the map that should be displayed
     map->setMapConfigPath("https://cdn.melown.com/mario/store/melown2015/"
             "map-config/melown/Melown-Earth-Intergeo-2017/mapConfig.json");
+
+    // acquire current time to measure how long each frame takes
+    uint32 lastRenderTime = SDL_GetTicks();
 
     // keep processing window events
     while (!shouldClose)
@@ -148,22 +149,27 @@ int main(int, char *[])
             }
         }
 
-        // update the map (both resources and rendering)
+        // update downloads
+        map->dataTick();
+
+        // update navigation etc.
+        uint32 currentRenderTime = SDL_GetTicks();
+        map->renderTickPrepare((currentRenderTime - lastRenderTime) * 1e-3);
+        lastRenderTime = currentRenderTime;
+
+        // prepare the rendering data
         updateResolution();
-        map->dataTick(); // update downloads
-        map->renderTickPrepare(); // update navigation etc.
-        map->renderTickRender(); // prepare the rendering data
+        map->renderTickRender();
 
         // actually render the map
-        vts::renderer::render(renderOptions, map->draws(),
-                              map->celestialBody());
+        render.render(map.get());
 
         // present the rendered image to the screen
         SDL_GL_SwapWindow(window);
     }
 
     // release all rendering related data
-    vts::renderer::finalize();
+    render.finalize();
 
     // release the map
     if (map)
